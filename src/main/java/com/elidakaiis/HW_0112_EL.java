2f565cce82b087508982ae23769fca2b68476a31
Elida Kais <kaiselida@gmail.com>
2025-12-03 15:46:18 +0600

    added HW_0112_EL

---
+++ src/main/java/com/elidakaiis/HW_0112_EL.java
@@ -0,0 +1,26 @@
+package com.elidakaiis;
+
+public class HW_0112_EL {
+    public static void main(String[] args) {
+        System.out.println(2 + 4);
+        System.out.println(4 - 2);
+        System.out.println(2 * 4);
+        System.out.println(4 / 2);
+
+        System.out.println(2.44 + 4);
+        System.out.println(2.44 * 4);
+        System.out.println(2 - 4.22);
+        System.out.println(2 / 4.22);
+
+        System.out.println(2 < 4);
+        System.out.println(2 > 4);
+        System.out.println(2 >= 4);
+        System.out.println(2 <= 4);
+
+        int x = 1000000000;
+        int y = 3;
+        int result = x * y;
+        System.out.println(result);
+    }
+}
+
